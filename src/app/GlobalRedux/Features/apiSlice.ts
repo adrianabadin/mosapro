798976b63acb850a9f6f99e2d4b00aebddb965c3
Adrian Abadin <aabadin@gmail.com>
2024-04-24 01:13:25 -0300

    fetch Issues

--- src/app/GlobalRedux/Features/apiSlice.ts
+++ src/app/GlobalRedux/Features/apiSlice.ts
@@ -41,10 +41,11 @@ export const loginSchema= z.object({
  */
 export type Login=z.infer<typeof loginSchema>
 export type LoginResponse = Login & {isAdmin:boolean}
-
+const dd=process.env.ORIGIN.trim()+"api"
+logger.error(dd)
 export const apiSlice=createApi({
     reducerPath: "api",
-    baseQuery:fetchBaseQuery({baseUrl:process.env.ORIGIN+"api",credentials:"include",mode:"same-origin"}),
+    baseQuery:fetchBaseQuery({baseUrl:process.env.ORIGIN.trim()+"api",credentials:"include",mode:"same-origin"}),
         tagTypes:[],refetchOnMountOrArgChange:true,
     endpoints:(builder)=>({
         uploadPhoto:builder.mutation<{driveId:string},FormData>({
